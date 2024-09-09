f53f67f672c87870812f6e0f369c2cea29dc708d
Alondra1752 <alondrasant7739@gmail.com>
2024-09-09 10:49:02 -0500

    worked on login page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,36 +1,36 @@
-import { Navigate, useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-
-import MonsterForm from "../components/OperationsForm";
-import MonsterList from "../components/LeaderboardList";
-
-import { QUERY_USER, QUERY_ME, QUERY_MONSTERS } from "../utils/queries";
-
-import Auth from "../utils/auth";
+import React from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER, QUERY_ME, QUERY_SCORES } from '../utils/queries'; 
+import Auth from '../utils/auth';
+import ScoreList from '../components/ScoreList'; // this assume's you have a component to display scores
+import ProfileForm from '../components/ProfileForm'; // this assume's you have a component to update profile
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
+  // Query to get user data or the logged-in user's data
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
-  const { loading: loadingFlashcard, data: FlashcardData } =
-    useQuery(QUERY_MONSTER);
+
+  // Query to get user scores
+  const { loading: loadingScores, data: scoresData } = useQuery(QUERY_SCORES);
 
   const user = data?.me || data?.user || {};
-  const monsters = ProfileData?.monsters || [];
+  const scores = scoresData?.scores || [];
 
-  // navigate to personal profile page if username is yours
+  // Redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Navigate to='/me' />;
   }
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div>Loading profile...</div>;
   }
 
-  if (loadingMonsters) {
-    return <div>Loading Monsters...</div>;
+  if (loadingScores) {
+    return <div>Loading scores...</div>;
   }
 
   if (!user) {
@@ -42,8 +42,8 @@ const Profile = () => {
     );
   }
 
-  if (monsters.length === 0) {
-    return <h4>No monsters to display</h4>;
+  if (scores.length === 0) {
+    return <h4>No scores to display</h4>;
   }
 
   return (
@@ -54,13 +54,14 @@ const Profile = () => {
         </h2>
 
         <div className='col-12 col-md-10 mb-5'>
-          <MonsterList monsters={monsters} title='Witcher Monsters' />
+          <ScoreList scores={scores} title='Your Quiz Scores' />
         </div>
+
         <div
           className='col-12 col-md-10 mb-3 p-3'
           style={{ border: "1px dotted #1a1a1a" }}
         >
-          <MonsterForm />
+          <ProfileForm user={user} />
         </div>
       </div>
     </div>
