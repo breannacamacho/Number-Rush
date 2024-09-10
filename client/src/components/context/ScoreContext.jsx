17b197452f27b06b796f5373a793c6c80fb2b83d
Reina2024 <simmsreina@gmail.com>
2024-09-10 11:43:40 -0400

    connection to backend fix

--- client/src/components/context/ScoreContext.jsx
+++ client/src/components/context/ScoreContext.jsx
@@ -10,22 +10,6 @@ export const ScoreProvider = ({ children }) => {
   const [score, setScore] = useState(0);
   const [leaderboard, setLeaderboard] = useState([]);
 
-<<<<<<< HEAD
-  const updateScore = (newScore) => {
-    setScore(newScore);
-  };
-
-  const updateLeaderboard = (newEntry) => {
-    setLeaderboard((prev) => [...prev, newEntry]);
-  };
-
-  return (
-    <ScoreContext.Provider value={{ score, leaderboard, updateScore, updateLeaderboard }}>
-      {children}
-    </ScoreContext.Provider>
-  );
-};
-=======
     const updateLeaderboard = (newEntry) => {
         setLeaderboard(prevLeaderboard => {
             const updatedLeaderboard = [...prevLeaderboard, newEntry].sort((a, b) => b.score - a.score).slice(0, 10); // Keeps top 10 scores
@@ -39,4 +23,3 @@ export const ScoreProvider = ({ children }) => {
         </ScoreContext.Provider>
     );
 };
->>>>>>> d9aa0462e826e16f404ae1bf116dce1ad92fc132
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+const cors = require('cors');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -21,8 +22,10 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
+  app.use('/graphql', 
+    cors({ origin: ['http://localhost:3000'] }),
+    expressMiddleware(server, {
+      context: authMiddleware,
   }));
 
   if (process.env.NODE_ENV === 'production') {
