f792ef7cd066dc70665505432ef6b598eb8468eb
Breanna Camacho <154536614+breannacamacho@users.noreply.github.com>
2024-09-12 15:48:41 -0400

    fixed for render

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/number-rush");
+mongoose.connect(
+  "mongodb+srv://breannacamacho1:<EiCi33daz2EsNt3y>@cluster0.fy7lz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" || "mongodb://127.0.0.1:27017/number-rush",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
